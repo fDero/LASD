2af36a1640085bb7166c9719962ac9c56b81c14d
fDero <francescodero@outlook.it>
2023-05-14 22:07:23 +0200

    hashtables fixed

--- hashtable/hashtable.cpp
+++ hashtable/hashtable.cpp
@@ -3,38 +3,21 @@
 
 #include "hashtable.hpp"
 #include <string>
+#include <concepts>
 
 namespace lasd {
 
     /******************************************** HASH-FUNCTIONS **************************************/
-    // notice that the hash function is a template method of the template class HashTable, it is instaciatied
-    // and specialized for types such as int, double and std::string, but it actually can be as generic as 
-    // needed with the use of a reinterpret cast wich is capable of reading the bytes of the spceific object
-    // passed as parameter and it treats them as if they were a string of character (each byte as a char)
+    // without loss of generality, every template-type is treated as a string, this allows for this
+    // one definition wich covers every use-case needed for this library without need for template-specialization
 
-    template<typename Data> inline sizetype HashTable<Data>::HashFunction(const Data& object) const noexcept {
-        auto ptr = reinterpret_cast<char const* const>(&object);
-        sizetype encoding = seed;
-        for (auto addresscursor = ptr; addresscursor - ptr < sizeof(Data); addresscursor++){
-            encoding = (encoding * 54059) ^ (*addresscursor * 76963);
-        }
-        return (encoding % 86969) % buckets;
-    }
-
-    template<> inline sizetype HashTable<std::string>::HashFunction(const std::string& string) const noexcept {
+    template<typename Data> sizetype HashTable<Data>::HashFunction(const Data& object) const noexcept {
+        std::string string;
+        if constexpr (std::same_as<Data, std::string>){ string = object; }
+        else { string = std::to_string(object); } 
         sizetype encoding = seed;
         for (const char letter : string) encoding = (encoding * 54059) ^ (76963 * (int)letter);
         return (encoding % 86969) % buckets;
-    }
-
-    template<> inline sizetype HashTable<int>::HashFunction(const int& integer) const noexcept {
-        return (integer * integer + seed) % buckets;
-    }
-
-    template<> inline sizetype HashTable<double>::HashFunction(const double& decimal_floating_point_value) const noexcept {
-        double square = (decimal_floating_point_value * decimal_floating_point_value);
-        sizetype encoding = ((sizetype)square + seed) % buckets;
-        return encoding;
     }
 
 
@@ -42,7 +25,7 @@ namespace lasd {
 
     /********************************** GET THE LOWEST POWER OF 2 BIGGER THAN nx *****************************/
     // this algorithms is based on the low level rappresentation of a numerical integral type in modern CPUs
-    // and archieves a constant-time-complexity computation of the lowest power of 2 bigger than a given number
+    // and archieves a O(1) time-complexity computation of the lowest power of 2 bigger than a given number
 
     template <typename Data> sizetype HashTable<Data>::RoundupPower2(sizetype nx) const noexcept {
         nx -= 0x00001;
